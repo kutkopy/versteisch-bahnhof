c5b1369645a88020b81450035ed673fdaece846d
Michael Stauffer <mich.stauffer@gmail.com>
2020-02-17 07:57:28 +0100

    Implemented REST interface

--- dialect_detection.py
+++ dialect_detection.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, precision_score, recall_score, f1_score
+from sklearn.pipeline import Pipeline
+import requests
+import json
 
 import mlflow
 import mlflow.sklearn
@@ -25,14 +28,13 @@ def load_data():
     test_data = pd.read_csv(TEST_DATA)
     test_data_label = pd.read_csv(TEST_DATA_LABEL)
 
-    train_phrases = train_data['Text'].values
-    test_phrases = test_data['Text'].values
+    train_phrases = train_data['Text'].values.tolist()
+    test_phrases = test_data['Text'].values.tolist()
 
-    train_labels = train_data['Label'].values
-    test_labels = test_data_label['Prediction'].values
+    train_labels = train_data['Label'].values.tolist()
+    test_labels = test_data_label['Prediction'].values.tolist()
 
-    train_tf_idf_features, test_tf_idf_features = get_tf_idf(train_phrases, test_phrases)
-    return train_tf_idf_features, train_labels, test_tf_idf_features, test_labels
+    return train_phrases, train_labels, test_phrases, test_labels
 
 
 def train_predict_model(classifier, train_features, train_labels, test_features):
@@ -53,8 +55,12 @@ def oracle():
     train_data, train_labels, test_data, test_labels = load_data()
 
     with mlflow.start_run():
+        tfidf_vectorizer = TfidfVectorizer(min_df=0.0, max_df=1.0, ngram_range=(1, 2), max_features=2000)
         random_forest_classifier = RandomForestClassifier()
-        predictions = train_predict_model(random_forest_classifier, train_data, train_labels, test_data)
+        pipeline = Pipeline([('tfidf', tfidf_vectorizer), ('clf', random_forest_classifier)])
+
+        pipeline.fit(train_data, train_labels)
+        predictions = pipeline.predict(test_data)
 
         precision, recall, f1 = get_metrics(test_labels, predictions)
 
@@ -62,8 +68,18 @@ def oracle():
         mlflow.log_metric("recall", recall)
         mlflow.log_metric("f1", f1)
 
-        mlflow.sklearn.log_model(random_forest_classifier, "model")
+        mlflow.sklearn.log_model(pipeline, "model")
+
+
+def predict_dialect():
+    url = "http://127.0.0.1:1234/invocations"
+    data = {
+        "columns": ["ja nei du seisch o", "jo nei du seisch au"]
+    }
+    headers = {'Content-type': 'application/json', 'format': 'pandas-split'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return r.content
 
 
 if __name__ == '__main__':
-    oracle()
+    print(predict_dialect())
